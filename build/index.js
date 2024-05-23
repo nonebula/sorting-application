f79160ef241c04956d4da5b929e593b046b53f35
Joe Higgs <higgs92@icloud.com>
2024-05-23 15:44:54 +0100

    Files built out, sort functions built

--- build/index.js
+++ build/index.js
@@ -1,32 +1,18 @@
 "use strict";
-// bubble sort algorithm and application
-// double nested 'for' loop
-class SortData {
-    constructor(data) {
-        this.data = data;
-    }
-    sort() {
-        const { length } = this.data;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                if (this.data[j] > this.data[j + 1]) {
-                    const left = this.data[j];
-                    this.data[j] = this.data[j + 1];
-                    this.data[j + 1] = left;
-                }
-            }
-        }
-    }
-}
+Object.defineProperty(exports, "__esModule", { value: true });
+const Strings_1 = require("./Strings");
 const dateArray = [
-    new Date(2020, 12, 1),
-    new Date(2022, 8, 13),
-    new Date(2014, 2, 22),
-    new Date(2009, 6, 15),
+    new Date(2200, 9, 24),
+    new Date(2035, 9, 24),
+    new Date(2001, 9, 24),
+    new Date(2029, 9, 24),
 ];
 const years = dateArray.map((date) => {
     return date.getFullYear();
 });
-const sortData = new SortData(years);
-sortData.sort();
-console.log(sortData.data);
+// const numbersData = new Numbers(years);
+// numbersData.sort();
+// console.log(numbersData.number);
+const stringsData = new Strings_1.Strings("vdGryF");
+stringsData.sort();
+console.log(stringsData.str);
